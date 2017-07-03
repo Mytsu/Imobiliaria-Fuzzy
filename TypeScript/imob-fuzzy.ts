cc8ac2a16c304c9d835d01e3ec532bc3a412f331
Jonathan Arantes <tlc.jooker@gmail.com>
2017-07-03 13:57:10 -0300

    Organize Files

---
+++ TypeScript/imob-fuzzy.ts
@@ -0,0 +1,23 @@
+﻿class Fuzzy {
+    static _INFINITY: number = 900000;
+
+    constructor() {}
+
+    min(l1: number, l2: number): number {
+        if (l1 < l2) {
+            return l1;
+        } else {
+            return l2;
+        }
+    }
+
+    minList(l: Array<number>): number {
+        let m: number = Fuzzy._INFINITY;
+        for (var i in l) {
+            if (m > +i) {
+                m = +i;
+            }
+        }
+        return m;
+    }
+}
